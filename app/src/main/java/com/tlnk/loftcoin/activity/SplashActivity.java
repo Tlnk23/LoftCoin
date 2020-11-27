9a5bc421cb86206c0dd9fd861fa34d98f4e95f28
tlnk <egorshinsasha@mail.ru>
2020-11-27 17:42:05 +0300

    some changes

--- app/src/main/java/com/tlnk/loftcoin/activity/SplashActivity.java
+++ app/src/main/java/com/tlnk/loftcoin/activity/SplashActivity.java
@@ -1,6 +1,12 @@
 package com.tlnk.loftcoin.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.preference.PreferenceManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +15,29 @@ import com.tlnk.loftcoin.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private SharedPreferences prefs;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    private Runnable goNext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean(WelcomeActivity.KEY_SHOW_WELCOME, true)) {
+            goNext = () -> startActivity(new Intent(this, WelcomeActivity.class));
+        } else {
+            goNext = () -> startActivity(new Intent(this, MainActivity.class));
+        }
+        handler.postDelayed(goNext, 1500);
+    }
+
+    @Override
+    protected void onStop() {
+        handler.removeCallbacks(goNext);
+        super.onStop();
     }
 }
